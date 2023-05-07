e6f062581633243426fc299bf0ff83df180347bb
Your Name <you@example.com>
2023-05-07 13:24:53 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import {Route, Routes} from 'react-router-dom';
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetupPages from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favourites";
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+
 function App() {
   return  ( 
-    <div>
-          <MainNavigation/>
+        <Layout>
           <Routes>
             <Route path="/" element={<AllMeetups />}></Route>
             <Route path="/new-meetup" element={<NewMeetupPages/>}></Route>
             <Route path="/favourites" element={<FavoritesPage/>}></Route>
-          </Routes> 
+          </Routes>
+        </Layout>
           
-  
-    </div>
+
 
   )
   
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -6,20 +6,20 @@ import Card from '../ui/Card';
 const MeetupItem = (props) => {
   return (
     <li className={classes.item}>
-      <Card>
-    <div className={classes.image}>
-        <img src={props.image} alt={props.title}/>
-    </div>
-    <div className={classes.content}>
+    <Card>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
         <p>{props.description}</p>
-    </div>
-    <div className={classes.action}>
-        <button>To favourite</button>
-    </div>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favorites</button>
+      </div>
     </Card>
-    </li>
+  </li>
   )
 }
 
