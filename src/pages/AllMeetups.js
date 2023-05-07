e068b6b87397a8d89a672df56d23aede1eaae79c
Your Name <you@example.com>
2023-05-07 23:23:00 +0600

    backendintrigated

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,62 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
-
+// const DUMMY_DATA = [
+//   {
+//     id: 'm1',
+//     title: 'This is a first meetup',
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+//   {
+//     id: 'm2',
+//     title: 'This is a second meetup',
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+// ];
+//async function  convert the function that return a promise 
 const AllMeetups = () => {
+  const [isLoading, setIsLoading] =useState(true);
+  const [loadedMeetups, setLoadedMeetup] =useState([])
+  useEffect(() =>{
+    setIsLoading(true);
+    fetch('https://react-started-38d49-default-rtdb.firebaseio.com/meetups.json')
+  .then(response =>{
+    return response.json();
+  }).then(data =>{
+    const meetups  =[];
+    for (const key in data){
+      const meetup ={
+        id: key,
+        ...data[key]
+
+      };
+      meetups.push(meetup);
+    }
+
+      setIsLoading(false);
+      setLoadedMeetup(meetups)
+  });
+
+  }, []);// external depandency should first render
+
+  if (isLoading) {
+    return <section>
+      <h2>Loading...</h2>
+    </section>
+  }
   return (
     <section>
         <h1>All_MeetUp</h1>
-        <MeetupList meetups={DUMMY_DATA}/>
+        <MeetupList meetups={loadedMeetups}/>
     </section>
   )
 }
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';//hoke provided by reaact, to navigate away once submitted
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetupPages = () => {
+  const navigate =useNavigate();
+  function addMeetupHandler(meetupData){
+    fetch('https://react-started-38d49-default-rtdb.firebaseio.com/meetups.json',
+    {
+      method: 'POST', // by default it will be get
+      body: JSON.stringify(meetupData),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    }
+    ).then(() =>{
+      navigate('/'); // leave that meetup page once we are done 
+
+    })
+
+// fatch () javascript function that allow us to send http request
+// add .json is what firebase required, not js
+// by default fatch send get request but we want 
+//we need post request to send the data and store
+  }
   return (
-    <div>NewMeetupPages</div>
+   <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+   </section>
   )
 }
 
